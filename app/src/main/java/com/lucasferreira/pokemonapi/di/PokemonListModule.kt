9f50647fe6f7c9ced2ed9082ec1f1faf3026ff59
Lucas Ferreira <luckeiros@users.noreply.github.com>
2021-01-20 16:17:23 -0300

    creating hilt module

---
+++ app/src/main/java/com/lucasferreira/pokemonapi/di/PokemonListModule.kt
@@ -0,0 +1,30 @@
+package com.lucasferreira.pokemonapi.di
+
+import com.lucasferreira.pokemonapi.feature.pokemonlist.repository.PokemonRepository
+import com.lucasferreira.pokemonapi.feature.pokemonlist.repository.PokemonRepositoryImpl
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+class PokemonListModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://pokeapi.co/api/v2/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    fun providePokemonRepository(retrofit: Retrofit): PokemonRepository {
+        return PokemonRepositoryImpl(retrofit)
+    }
+}
--- app/src/main/java/com/lucasferreira/pokemonapi/feature/pokemonlist/repository/PokemonRepositoryImpl.kt
+++ app/src/main/java/com/lucasferreira/pokemonapi/feature/pokemonlist/repository/PokemonRepositoryImpl.kt
@@ -1,4 +1,10 @@
 package com.lucasferreira.pokemonapi.feature.pokemonlist.repository
 
-class PokemonRepositoryImpl {
+import retrofit2.Retrofit
+import javax.inject.Inject
+
+class PokemonRepositoryImpl @Inject constructor(private val retrofit: Retrofit) :
+    PokemonRepository {
+
+
 }
