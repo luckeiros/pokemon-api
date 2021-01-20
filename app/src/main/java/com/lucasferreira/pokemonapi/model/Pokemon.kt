9e15f7da53df2930e2c00a2c54176b9a72abe23b
Lucas Ferreira <luckeiros@users.noreply.github.com>
2021-01-20 16:42:33 -0300

    creating model

---
+++ app/src/main/java/com/lucasferreira/pokemonapi/model/Pokemon.kt
@@ -0,0 +1,6 @@
+package com.lucasferreira.pokemonapi.model
+
+data class Pokemon(
+    val name: String,
+    val url: String
+)
---
+++ app/src/main/java/com/lucasferreira/pokemonapi/model/PokemonResponse.kt
@@ -0,0 +1,5 @@
+package com.lucasferreira.pokemonapi.model
+
+data class PokemonResponse(
+    val pokemonList: List<Pokemon>
+)
