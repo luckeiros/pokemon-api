72fc899978b16fca5ce889a7ca99ee2f4cbba559
Lucas Ferreira <luckeiros@users.noreply.github.com>
2021-01-22 02:01:25 -0300

    view changes and add limit and offset query

--- app/src/main/java/com/lucasferreira/pokemonapi/feature/pokemonlist/repository/service/PokemonService.kt
+++ app/src/main/java/com/lucasferreira/pokemonapi/feature/pokemonlist/repository/service/PokemonService.kt
@@ -3,10 +3,10 @@ package com.lucasferreira.pokemonapi.feature.pokemonlist.repository.service
 import com.lucasferreira.pokemonapi.di.Endpoint
 import com.lucasferreira.pokemonapi.model.PokemonResponse
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface PokemonService {
     @GET(Endpoint.POKEMON_ENDPOINT)
-    suspend fun getPokemon() : PokemonResponse
+    suspend fun getPokemon(@Query("limit") limit: Int, @Query("offset") offset: Int): PokemonResponse
 
 }
