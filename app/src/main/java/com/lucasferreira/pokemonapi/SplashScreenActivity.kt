8745670b7cf0b1e18a6ddf79b15ad613df9f7fc6
Lucas Ferreira <luckeiros@users.noreply.github.com>
2021-01-23 20:18:10 -0300

    making some view changes

---
+++ app/src/main/java/com/lucasferreira/pokemonapi/SplashScreenActivity.kt
@@ -0,0 +1,30 @@
+package com.lucasferreira.pokemonapi
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.lucasferreira.pokemonapi.feature.pokemonlist.view.PokemonListActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        setOpenTime()
+    }
+
+    private fun openPokemonListActivity() {
+        val intent = Intent(this, PokemonListActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun setOpenTime() {
+        val handler = Handler()
+        handler.postDelayed({
+            openPokemonListActivity()
+        }, 2500)
+
+    }
+}
--- app/src/main/java/com/lucasferreira/pokemonapi/extension/ViewExt.kt
+++ app/src/main/java/com/lucasferreira/pokemonapi/extension/ViewExt.kt
@@ -3,8 +3,6 @@ package com.lucasferreira.pokemonapi.extension
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
-import androidx.recyclerview.widget.RecyclerView
-import java.util.*
 
 
 fun View.turnVisible() {
