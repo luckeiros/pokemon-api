3642b37a5b811d41be6af80ff9266d96dce24b86
Lucas Ferreira <luckeiros@users.noreply.github.com>
2021-01-24 07:25:20 -0300

    strings renamed

--- app/src/main/java/com/lucasferreira/pokemonapi/extension/StringExt.kt
+++ app/src/main/java/com/lucasferreira/pokemonapi/extension/StringExt.kt
@@ -2,15 +2,15 @@ package com.lucasferreira.pokemonapi.extension
 
 import java.text.DecimalFormat
 
-private fun dm() = DecimalFormat().apply { maximumFractionDigits = 1 }
+private fun decimalFormat() = DecimalFormat().apply { maximumFractionDigits = 1 }
 
 fun Int.formatDecimeter() =
     if (this >= 10) {
-        dm().format(div(10.0)) + "m"
+        decimalFormat().format(div(10.0)) + "m"
     } else "${times(10)} cm"
 
 fun Int.formatKilogram() =
     if (this >= 10) {
-        dm().format(div(10f)) + "Kg"
+        decimalFormat().format(div(10f)) + "Kg"
     } else "${times(100)} g"
 
